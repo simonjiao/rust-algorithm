e977f53a3690ede4b86863649b45ecd9ebfa2193
simonjiaoh <simonjiaoh@gmail.com>
2021-05-01 01:21:23 +0800

    lt 252 update

--- src/bin/meeting_rooms.rs
+++ src/bin/meeting_rooms.rs
@@ -22,7 +22,23 @@ fn can_attend_meeting(intervals: Vec<Vec<i32>>) -> bool {
     true
 }
 
+fn can_attend_meeting_ii(mut intervals: Vec<Vec<i32>>) -> bool {
+    intervals.sort_unstable_by(|a, b| a[0].partial_cmp(&b[0]).unwrap());
+    let mut idx = 1;
+    while idx < intervals.len() {
+        if overlap(&intervals[idx - 1], &intervals[idx]) {
+            return false;
+        }
+        idx += 1;
+    }
+    true
+}
+
 fn main() {
     let intervals = vec![vec![0, 30], vec![5, 10], vec![15, 20]];
     assert_eq!(can_attend_meeting(intervals), false);
+
+    //let mut intervals: Vec<Meeting> = vec![Meeting::new(13,15), Meeting::new(1,13)];
+    let intervals = vec![vec![13, 15], vec![1, 13]];
+    assert_eq!(can_attend_meeting_ii(intervals), true);
 }
