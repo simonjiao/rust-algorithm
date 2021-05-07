5be950f31ead092a9ba0dc78773cafaeaf09e83f
simonjiao <simon_jiaoh@hotmail.com>
2021-05-07 15:09:42 +0800

    update lt 1486

--- src/bin/lt1486.rs
+++ src/bin/lt1486.rs
@@ -1,5 +1,3 @@
-use std::borrow::Borrow;
-
 #[allow(dead_code)]
 fn xor_operation1(n:i32, start:i32) -> i32 {
     let mut val = 0;
@@ -9,20 +7,19 @@ fn xor_operation1(n:i32, start:i32) -> i32 {
     val
 }
 
-fn sum_xor(x:i32) -> Option<i32> {
-    match x.clone() % 4 {
-        0 => Some(x),
-        1 => Some(1),
-        2 => Some(x + 1),
-        3 => Some(0),
-        _ => None,
+fn sum_xor(x:i32) -> i32 {
+    match &x % 4 {
+        0 => x,
+        1 => 1,
+        2 => x + 1,
+        _ => 0,
     }
 }
 
 fn xor_operation(n:i32, start:i32) -> i32 {
-    let s = start.borrow() >> 1;
-    let e = n & start & 1;
-    let res = sum_xor(s - 1).unwrap() ^ sum_xor(s + n -1).unwrap();
+    let s = &start >> 1;
+    let e = &n & start & 1;
+    let res = sum_xor(s - 1) ^ sum_xor(s + n -1);
     res << 1 | e
 }
 
